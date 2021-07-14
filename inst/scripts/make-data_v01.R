37bbf98e6110f28178f3455086c6917a5b122f7d
kumeS <satoshi.kume@riken.jp>
2021-07-14 16:31:37 +0900

    v-1.0.4

--- inst/scripts/make-data_v01.R
+++ inst/scripts/make-data_v01.R
@@ -8,21 +8,101 @@ library(magrittr)
 library(filesstrings)
 library(animation)
 
-
 ################################################################
 #Convert the images to the array data in R
 ################################################################
 ################################################################
-#SampleDataset_cats_segmentation
+#EM_id0001_Brain_CA1_hippocampus_region
 ################################################################
 #Set parameters
-DataFolder <- "SampleDataset_cats_segmentation"
-DataPath <- "./BioImageDbs_02_Dataset_v02"
-WIDTH00 <- 128; HEIGHT00 <- 128; CHANNELS01 <- 3; CHANNELS02 <- 1
+DataFolder <- "EM_id0001_Brain_CA1_hippocampus_region"
+Ori <- "OriginalData"
+GT <- "mitochondria_GT"
+WIDTH00 <- 1024; HEIGHT00 <- 768; CHANNELS00 <- 1
+
+#Run the conversion
+DataImport_3d_seg(WIDTH = WIDTH00, HEIGHT = HEIGHT00, Z=-1, CHANNELS = CHANNELS00,
+           data="./BioImageDbs_Data",
+           path01=DataFolder,
+           Original_path=Ori,
+           GroundTruth_path=GT,
+           FileName=paste0(DataFolder, "_5dTensor"))
+
+filesstrings::file.move(files=paste0(DataFolder, "_5dTensor.Rds"),
+                        destinations="./BioImageDbs_Output",
+                        overwrite = TRUE)
+
+Dat <- readRDS( paste0("./BioImageDbs_Output/", DataFolder, "_5dTensor.Rds") )
+#str(Dat); str(Dat$Train)
+for(n in 1:dim(Dat$Train$Train_Original)[1]){
+Sample <- paste0(DataFolder, "_5dTensor_train_dataset_",  n)
+ImageView3D(Dat$Train, Name=, Sample=n, Interval = 0.5)
+filesstrings::file.move(files=paste0(Sample, ".gif"), destinations="./DL_analysis_v01",
+                        overwrite = TRUE)
+}
+
+################################################################
+#EM_id0002_Drosophila_brain_region
+################################################################
+#Set parameters
+DataFolder <- "EM_id0002_Drosophila_brain_region"
+WIDTH00 <- 512; HEIGHT00 <- 512; CHANNELS00 <- 1
+
+#Run the conversion
+DataImport_3d_seg(WIDTH = WIDTH00, HEIGHT = HEIGHT00, Z=-1, CHANNELS = CHANNELS00,
+           data="./BioImageDbs_Data",
+           path01=DataFolder,
+           Original_path="OriginalData",
+           GroundTruth_path="membrane_GT_inv",
+           OriginalDataOnlyinTest=TRUE,
+           FileName=paste0(DataFolder, "_5dTensor"))
+
+filesstrings::file.move(files=paste0(DataFolder, "_5dTensor.Rds"),
+                        destinations="./BioImageDbs_Output",
+                        overwrite = TRUE)
+Dat <- readRDS( paste0("./BioImageDbs_Output/", DataFolder, "_5dTensor.Rds") )
+str(Dat); str(Dat$Train)
+ImageView3D(Dat$Train, Interval=0.4, Name=paste0(DataFolder, "_5dTensor_train_dataset"))
+filesstrings::file.move(files=paste0(DataFolder, "_5dTensor_train_dataset.gif"),
+                        destinations="./BioImageDbs_Output",
+                        overwrite = TRUE)
+
+################################################################
+#EM_id0003_Drosophila_brain_region
+################################################################
+#Set parameters
+DataFolder <- "EM_id0003_J558L"
+WIDTH00 <- 1024; HEIGHT00 <- 1024; CHANNELS00 <- 1
 
 #Run the conversion
 DataImport_2d_seg(WIDTH = WIDTH00, HEIGHT = HEIGHT00, CHANNELS = CHANNELS00,
-                  data=DataPath,
+                  data="./BioImageDbs_Data",
+                  path01=DataFolder,
+                  Original_path="OriginalData",
+                  GroundTruth_path="GroundTruth_8b",
+                  FileName=paste0(DataFolder, "_4dTensor"))
+
+filesstrings::file.move(files=paste0(DataFolder, "_4dTensor.Rds"),
+                        destinations="./BioImageDbs_Output",
+                        overwrite = TRUE)
+Dat <- readRDS( paste0("./BioImageDbs_Output/", DataFolder, "_4dTensor.Rds") )
+#str(Dat); str(Dat$Train)
+#table(Dat$Train$Train_GroundTruth)
+Dat$Train$Train_GroundTruth[Dat$Train$Train_GroundTruth < 0.4] <- 0
+ImageView2D(Dat$Train, Interval=0.8, Name=paste0(DataFolder, "_4dTensor_train_dataset"))
+filesstrings::file.move(files=paste0(DataFolder, "_4dTensor_train_dataset.gif"),
+                        destinations="./BioImageDbs_Output",
+                        overwrite = TRUE)
+
+################################################################
+#EM_id0004_PrHudata
+################################################################
+#Set parameters
+DataFolder <- "EM_id0004_PrHudata"
+WIDTH00 <- 1024; HEIGHT00 <- 1024; CHANNELS00 <- 1
+#Run the conversion
+DataImport_2d_seg(WIDTH = WIDTH00, HEIGHT = HEIGHT00, CHANNELS = CHANNELS00,
+                  data="./BioImageDbs_Data",
                   path01=DataFolder,
                   Original_path="OriginalData",
                   GroundTruth_path="GroundTruth_8b",
